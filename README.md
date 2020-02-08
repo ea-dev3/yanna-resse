7680fbf8442d59c96b5d10440435c756858c30ac
ea.dev3 <ea.dev3@gmail.com>
2020-02-08 00:24:17 -0800

    Minor Ui tweaks

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-Tyra - Feminine Blog Powered by Gatsby
-=====
+# Yanna Resse - Feminine Blog Powered by Gatsby
+
 [![Contributions Welcome](https://img.shields.io/badge/Contributions-Welcome-green.svg)](https://shields.io/) [![MIT License](https://img.shields.io/badge/License-MIT-blue.svg)](https://shields.io/)
 
-Tyra is a Gatsby starter that focuses on legibility and a clean, feminine design with 2010 influences. Technically, the goals for Tyra are having a "plug and play" blog, with great SEO, tooling, and integrations all included.
+Yanna Resse is a Gatsby starter that focuses on legibility and a clean, feminine design with 2010 influences. Technically, the goals for Yanna Resse are having a "plug and play" blog, with great SEO, tooling, and integrations all included.
 
 [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/madelyneriksen/gatsby-starter-tyra)
 
 ## Getting Started
 
-The first step with using Tyra is customizing your metadata in `gatsby-config.js`.
+The first step with using Yanna Resse is customizing your metadata in `gatsby-config.js`.
 
 ```javascript
 siteMetadata: {
@@ -35,31 +35,33 @@ This is used for configuring data across the site, improving SEO, and increasing
 Next up, you can begin customizing the images and copy in `content/`. Finally, you can add your own blog posts! Posts are written in Markup, an easy way to create webpages.
 
 ## Installation with `gatsby-cli`
+
 `gatsby new my-site https://github.com/madelyneriksen/gatsby-starter-tyra`
 
 ## Installation with `git clone`
+
 `git clone git@github.com:madelyneriksen/gatsby-starter-tyra.git my-site`
 `cd my-site`
 `yarn install`
 
 ## To develop
+
 `yarn develop`
 
 ## To build
-`yarn build`
 
+`yarn build`
 
 ## Screenshot
 
-![Screenshot of Tyra Homepage](./screenshot.jpg)
+![Screenshot of Yanna Resse  Homepage](./screenshot.jpg)
 
 ## Adding your Content
 
-Tyra uses markdown for writing blog posts, and follows a simple template for adding new posts. To add a new blog post, create a file in `content/posts/` with the following:
+Yanna Resse uses markdown for writing blog posts, and follows a simple template for adding new posts. To add a new blog post, create a file in `content/posts/` with the following:
 
 ```markdown
 ---
-
 type: "post"
 title: "My Awesome Post Title"
 author: "My Name"
@@ -68,10 +70,7 @@ date: "2019-01-05"
 slug: "/my-awesome-post"
 postImage: "./img/myimage.jpg"
 metaDescription: "This is my first awesome and cool post!!!"
-
----
-
-You can write your post here using markdown! Link to images in the `img` folder using this syntax:
+---You can write your post here using markdown! Link to images in the `img` folder using this syntax:
 
 ![Alt Text](./img/my-image.jpeg)
 ```
@@ -80,10 +79,10 @@ Images for posts are stored in `content/posts/img/`. Images in the frontmatter w
 
 ## License
 
-Tyra is licensed under the terms of the MIT License.
+Yanna Resse is licensed under the terms of the MIT License.
 
 ## Thanks and Mentions
 
-* The [GatsbyJS](https://www.gatsbyjs.org/) Team for creating an amazing platform.
-* [Netlify](https://www.netlify.com/) for making static site hosting even easier.
-* [Unsplash](https://unsplash.com/) and the Free Photography Community.
+- The [GatsbyJS](https://www.gatsbyjs.org/) Team for creating an amazing platform.
+- [Netlify](https://www.netlify.com/) for making static site hosting even easier.
+- [Unsplash](https://unsplash.com/) and the Free Photography Community.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,34 @@
 module.exports = {
-  siteMetadata: {
-    navbarLinks: [
-      {to: "/makeup", name: "Makeup"},
-      {to: "/lifestyle", name: "Lifestyle"},
-      {to: "/blog", name: "blog"},
-    ],
-    title: "TYRA",
-    description: "Tyra is a fast, feminine, and chic Gatsby.js theme.",
-    siteUrl: "https://tyra-starter.netlify.com",
-    homepageHeader: "Welcome to Your New Blog",
-    homepageAbout: "Tyra is a modern, sleek and feminine Gatsby.js theme. Easily create a beautiful and fast blog and draw attention to your stellar content.",
-    mailChimpUrl: "https://mailchimp.com",
-    mailChimpToken: "MAILCHIMP TOKEN HERE",
-    youtube: "", // YOUR YOUTUBE PROFILE HERE
-    github: "", // YOUR GITHUB PROFILE HERE 
-    pinterest: "", // YOUR PINTEREST PROFILE HERE
-    facebook: "", // YOUR FACEBOOK PROFILE HERE
-    twitter: "", // YOUR TWITTER PROFILE HERE
-  },
-  plugins: [
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
+	siteMetadata: {
+		navbarLinks: [
+			{ to: "/makeup", name: "Makeup" },
+			{ to: "/lifestyle", name: "Lifestyle" },
+			{ to: "/blog", name: "blog" }
+		],
+		title: "Yanna Resse",
+		description:
+			"Welcome to my blog Yanna Resse. I'm gonna be using this platform to help you to make decisions in cosmetics and lifestyle. I will be making recommendations based on personal experience, giving you insights on what you should look out for and ehat needs improvement. This is an open forum for you guys to share your experiences. Feel at home",
+		siteUrl: "https://yanna-resse.netlify.com/",
+		homepageHeader: "Hello Y'all ",
+		homepageAbout:
+			"Welcome to my blog Yanna Resse. I'm gonna be using this platform to help you to make decisions in cosmetics and lifestyle. I will be making recommendations based on personal experience, giving you insights on what you should look out for and ehat needs improvement. This is an open forum for you guys to share your experiences. Feel at home",
+		mailChimpUrl: "https://mailchimp.com",
+		mailChimpToken: "MAILCHIMP TOKEN HERE",
+		youtube: "", // YOUR YOUTUBE PROFILE HERE
+		github: "", // YOUR GITHUB PROFILE HERE
+		pinterest: "", // YOUR PINTEREST PROFILE HERE
+		facebook: "", // YOUR FACEBOOK PROFILE HERE
+		twitter: "" // YOUR TWITTER PROFILE HERE
+	},
+	plugins: [
+		"gatsby-plugin-sitemap",
+		"gatsby-plugin-react-helmet",
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-sharp",
+		{
+			resolve: "gatsby-plugin-feed",
+			options: {
+				query: `
         {
           site {
             siteMetadata {
@@ -38,20 +40,20 @@ module.exports = {
           }
         }
       `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.edges.map(edge => {
+								return Object.assign({}, edge.node.frontmatter, {
+									description: edge.node.excerpt,
+									date: edge.node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+									guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+									custom_elements: [{ "content:encoded": edge.node.html }]
+								});
+							});
+						},
+						query: `
             {
               allMarkdownRemark(
                 limit: 1000,
@@ -72,51 +74,51 @@ module.exports = {
               }
             }
           `,
-            output: "/rss.xml",
-            title: "Gatsby RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content`,
-        name: 'content',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1400,
-            },
-          },
-        ],
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Karla', 'Playfair Display', 'Lora']
-        }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: "",
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        exclude: ['/success'],
-        cookieDomain: "tyra-starter.netlify.com",
-      }
-    }
-  ]
-}
+						output: "/rss.xml",
+						title: "RSS Feed"
+					}
+				]
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/content`,
+				name: "content"
+			}
+		},
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					"gatsby-remark-copy-linked-files",
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							maxWidth: 1400
+						}
+					}
+				]
+			}
+		},
+		{
+			resolve: "gatsby-plugin-web-font-loader",
+			options: {
+				google: {
+					families: ["Karla", "Playfair Display", "Lora"]
+				}
+			}
+		},
+		{
+			resolve: "gatsby-plugin-google-analytics",
+			options: {
+				trackingId: "",
+				head: false,
+				anonymize: true,
+				respectDNT: true,
+				exclude: ["/success"],
+				cookieDomain: "yanna-resse.netlify.com"
+			}
+		}
+	]
+};
